dc088d6a448273ce3287a6b04d16fd0ec06f4803
littletwoblack <1063819522@qq.com>
2014-10-27 14:48:08 +0800

    第四题获得完整purchase信息

--- pos_v1/src/main.js
+++ pos_v1/src/main.js
@@ -68,11 +68,37 @@ for (var j in inputs)
 
 }
 
-
-
-
 //程序执行到这里的时候，purchase数组已经存储了所有购买商品的条码号和数量
+    var allItems=loadAllItems()
+    for(var lookbar in purchase)
+    {
+        for (var i in allItems)
+        {
+            if(purchase[lookbar].barcode==allItems[i].barcode)
+            {
+                purchase[lookbar].name=allItems[i].name  ;
+                purchase[lookbar].unit=allItems[i].unit;
+                purchase[lookbar].price=allItems[i].price;
+            }
+        }
+    }//去商品集合中通过条码号查找出所有完整的信息
+
+    var sum=0;
+    var string;
+    string='***<没钱赚商店>购物清单***\n' ;
+
+    for(var i in purchase)
+    {
+        string+='名称：'+purchase[i].name+'，数量：'+purchase[i].count+purchase[i].unit+'，单价：'+purchase[i].price.toFixed(2)
+            +'(元)，小计：'+(purchase[i].price*purchase[i].count).toFixed(2)+'(元)\n'
+
+        sum+=purchase[i].price*purchase[i].count
+    }
+    string+='----------------------\n'
+    string+='总计：'+sum.toFixed(2)+'(元)\n'
+    string+='**********************'
+    console.log(string)
 
 
-
+    
 }
